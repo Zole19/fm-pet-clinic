a9ab810d47ab38eb69bfdd09351e6c83026d8bdf
Zole19 <filipmustic123@gmail.com>
2021-08-25 11:54:58 +0200

    Initial Commit

---
+++ src/main/java/com/example/fmpetclinic/FmPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.example.fmpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FmPetClinicApplication.class, args);
+    }
+
+}
